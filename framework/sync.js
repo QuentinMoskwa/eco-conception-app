bf7238590d6e6e7fd34a6a42d2e34f13511bfa5c
Moskwa Quentin moskwaquentin@gmail.com <95235691+QuentinMoskwa@users.noreply.github.com>
2025-03-14 12:59:08 +0100

    adding force sync.

--- framework/sync.js
+++ framework/sync.js
@@ -2,7 +2,7 @@ const {bdd} = require('./connexion.js');
 const Product = require('../model/product.model.js');
 
 const sync = async () => {
-    await bdd.sync();
+    await bdd.sync({force : true});
 }
 
 module.exports = sync;
